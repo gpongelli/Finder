81d7fed248bac28934d2f9428e444be0dbcec6eb
Michael Babker <mbabker@flbab.com>
2011-08-12 22:43:06 +0430

    CodeSniffer and query work

--- com_finder/admin/tables/link.php
+++ com_finder/admin/tables/link.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @version		$Id: link.php 981 2010-06-15 18:38:02Z robs $
- * @package		JXtended.Finder
- * @subpackage	com_finder
- * @copyright	Copyright (C) 2007 - 2010 JXtended, LLC. All rights reserved.
- * @license		GNU General Public License
+ * @package     Joomla.Administrator
+ * @subpackage  com_finder
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 defined('_JEXEC') or die;
@@ -14,48 +14,21 @@ jimport('joomla.application.component.model');
 /**
  * Link table class for the Finder package.
  *
- * @package		JXtended.Finder
- * @subpackage	com_finder
- * @version		1.1
+ * @package     Joomla.Administrator
+ * @subpackage  com_finder
+ * @since       2.5
  */
 class FinderTableLink extends JTable
 {
-	var $link_id			= null;
-
-	var $url				= null;
-
-	var $route				= null;
-
-	var $title				= null;
-
-	var $description		= null;
-
-	var $fulltxt			= null;
-
-	var $indexdate			= null;
-
-	var $size				= null;
-
-	var $md5sum				= null;
-
-	var $state				= null;
-
-	var $access				= null;
-
-	var $language			= null;
-
-	var $publish_start_date	= null;
-
-	var $publish_end_date	= null;
-
-	var $start_date			= null;
-
-	var $end_date			= null;
-
-	var $type_id			= null;
-
-	var $adapter_id			= null;
-
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$db  JDatabase connector object.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 */
 	function __construct(&$db)
 	{
 		parent::__construct('#__finder_links', 'link_id', $db);
--- com_finder/admin/tables/map.php
+++ com_finder/admin/tables/map.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @package		JXtended.Finder
- * @subpackage	com_finder
- * @copyright	Copyright (C) 2007 - 2010 JXtended, LLC. All rights reserved.
- * @license		GNU General Public License
+ * @package     Joomla.Administrator
+ * @subpackage  com_finder
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 defined('_JEXEC') or die;
@@ -13,12 +14,21 @@ jimport('joomla.application.component.model');
 /**
  * Map table class for the Finder package.
  *
- * @package		JXtended.Finder
- * @subpackage	com_finder
- * @version		1.1
+ * @package     Joomla.Administrator
+ * @subpackage  com_finder
+ * @since       2.5
  */
 class FinderTableMap extends JTable
 {
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$db  JDatabase connector object.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 */
 	function __construct(&$db)
 	{
 		parent::__construct('#__finder_taxonomy', 'id', $db);
@@ -29,14 +39,14 @@ class FinderTableMap extends JTable
 	 * table. The method respects checked out rows by other users and will attempt
 	 * to checkin rows that it can after adjustments are made.
 	 *
-	 * @param   mixed    $pks      An optional array of primary key values to update.  If not
+	 * @param   mixed    $pks     An optional array of primary key values to update.  If not
 	 *                            set the instance property value is used.
 	 * @param   integer  $state   The publishing state. eg. [0 = unpublished, 1 = published]
 	 * @param   integer  $userId  The user id of the user performing the operation.
 	 *
 	 * @return  boolean  True on success.
 	 *
-	 * @since   11.1
+	 * @since   2.5
 	 */
 	public function publish($pks = null, $state = 1, $userId = 0)
 	{
@@ -49,12 +59,15 @@ class FinderTableMap extends JTable
 		$state  = (int) $state;
 
 		// If there are no primary keys set check to see if the instance key is set.
-		if (empty($pks)) {
-			if ($this->$k) {
+		if (empty($pks))
+		{
+			if ($this->$k)
+			{
 				$pks = array($this->$k);
 			}
 			// Nothing to set publishing state on, return false.
-			else {
+			else
+			{
 				$this->setError(JText::_('JLIB_DATABASE_ERROR_NO_ROWS_SELECTED'));
 				return false;
 			}
@@ -64,22 +77,23 @@ class FinderTableMap extends JTable
 		$where = $k.'='.implode(' OR '.$k.'=', $pks);
 
 		// Update the publishing state for rows with the given primary keys.
-		$this->_db->setQuery(
-			'UPDATE '.$this->_db->quoteName($this->_tbl).
-			' SET '.$this->_db->quoteName('state').' = '.(int) $state .
-			' WHERE ('.$where.')' .
-			$checkin
-		);
+		$query = $this->_db->getQuery(true);
+		$query->update($this->_db->quoteName($this->_tbl));
+		$query->set($this->_db->quoteName('state').' = '.(int) $state);
+		$query->where($where);
+		$this->_db->setQuery($query);
 		$this->_db->query();
 
 		// Check for a database error.
-		if ($this->_db->getErrorNum()) {
+		if ($this->_db->getErrorNum())
+		{
 			$this->setError($this->_db->getErrorMsg());
 			return false;
 		}
 
 		// If the JTable instance value is in the list of primary keys that were set, set the instance.
-		if (in_array($this->$k, $pks)) {
+		if (in_array($this->$k, $pks))
+		{
 			$this->state = $state;
 		}
 
