86760b267cce8d30be258e01e28c2b1f241ed21f
Michael Babker <mbabker@flbab.com>
2011-09-05 15:05:47 +0430

    CodeSniffer

--- plg_system_finder/finder.php
+++ plg_system_finder/finder.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @version		$Id: finder.php 981 2010-06-15 18:38:02Z robs $
- * @package		JXtended.Finder
- * @subpackage	plgSystemFinder
- * @copyright	Copyright (C) 2007 - 2010 JXtended, LLC. All rights reserved.
- * @license		GNU General Public License
+ * @package     Joomla.Plugin
+ * @subpackage  System.Finder
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 defined('JPATH_BASE') or die;
@@ -12,8 +12,9 @@ defined('JPATH_BASE') or die;
 /**
  * System plugin class for Finder.
  *
- * @package		JXtended.Finder
- * @subpackage	plgSystemFinder
+ * @package     Joomla.Plugin
+ * @subpackage  System.Finder
+ * @since       2.5
  */
 class plgSystemFinder extends JPlugin
 {
@@ -24,22 +25,27 @@ class plgSystemFinder extends JPlugin
 	 * search results. The highlighting is done with JavaScript so we just
 	 * need to check a few parameters and the JHtml behavior will do the rest.
 	 *
-	 * @return	boolean		True on success.
+	 * @return  boolean  True on success
+	 *
+	 * @since   2.5
 	 */
 	public function onAfterDispatch()
 	{
 		// Check that we are in the site application.
-		if (JFactory::getApplication()->isAdmin()) {
-			return true;
-		}
-
-		// Check if the highlighter should be activated in this environment.
-		if (JFactory::getDocument()->getType() !== 'html' || JRequest::getCmd('tmpl') === 'component') {
+		if (JFactory::getApplication()->isAdmin())
+		{
 			return true;
 		}
 
 		// Check if the highlighter is enabled.
-		if (!JComponentHelper::getParams('com_finder')->get('highlight_content_search_terms', 1)) {
+		if (!JComponentHelper::getParams('com_finder')->get('highlight_content_search_terms', 1))
+		{
+			return true;
+		}
+
+		// Check if the highlighter should be activated in this environment.
+		if (JFactory::getDocument()->getType() !== 'html' || JRequest::getCmd('tmpl') === 'component')
+		{
 			return true;
 		}
 
@@ -48,7 +54,8 @@ class plgSystemFinder extends JPlugin
 		$terms = $terms ? @unserialize(@base64_decode($terms)) : null;
 
 		// Check the terms.
-		if (empty($terms)) {
+		if (empty($terms))
+		{
 			return true;
 		}
 
--- plg_system_finder/script.php
+++ plg_system_finder/script.php
@@ -1,44 +1,54 @@
 <?php
 /**
- * @package		JXtended.Finder
- * @subpackage	plgFinderJoomla_Articles
- * @copyright	Copyright (C) 2007 - 2010 JXtended, LLC. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
- * @link		http://jxtended.com
+ * @package     Joomla.Plugin
+ * @subpackage  System.Finder
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
 /**
  * Installation class to perform additional changes during install/uninstall/update
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  System.Finder
+ * @since       2.5
  */
-class plgSystemFinderInstallerScript {
-
+class plgSystemFinderInstallerScript
+{
 	/**
 	 * Function to perform changes when plugin is initially installed
 	 *
-	 * @param	$parent
+	 * @param   object  $parent  The parent object of the script
 	 *
-	 * @return	void
-	 * @since	1.6
+	 * @return  void
+	 *
+	 * @since   2.5
 	 */
-	function install($parent) {
+	function install($parent)
+	{
 		$this->activateButton();
 	}
 
 	/**
 	 * Function to activate the button at installation
 	 *
-	 * @return	void
-	 * @since	1.7
+	 * @return  void
+	 *
+	 * @since   2.5
 	 */
-	function activateButton() {
+	function activateButton()
+	{
 		$db = JFactory::getDBO();
 		$query	= $db->getQuery(true);
 		$query->update($db->quoteName('#__extensions'));
 		$query->set($db->quoteName('enabled').' = 1');
 		$query->where($db->quoteName('name').' = '.$db->quote('plg_system_finder'));
 		$db->setQuery($query);
-		if (!$db->query()) {
+		if (!$db->query())
+		{
 			JError::raiseNotice(1, JText::_('PLG_SYSTEM_FINDER_ERROR_ACTIVATING_PLUGIN'));
 		}
 	}
 }
+
