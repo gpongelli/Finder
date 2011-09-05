a701caba08ec19e6e56c35c99854ebe7c91ca70a
Michael Babker <mbabker@flbab.com>
2011-09-05 16:38:47 +0430

    Add start_date for queries

--- plg_finder_joomla_newsfeeds/joomla_newsfeeds.php
+++ plg_finder_joomla_newsfeeds/joomla_newsfeeds.php
@@ -307,7 +307,7 @@ class plgFinderJoomla_Newsfeeds extends FinderIndexerAdapter
 		// Check if we can use the supplied SQL query.
 		$sql = is_a($sql, 'JDatabaseQuery') ? $sql : $db->getQuery(true);
 		$sql->select('a.id, a.catid, a.name AS title, a.alias, a.link AS link');
-		$sql->select('a.published AS state, a.ordering, a.publish_up AS start_date, a.params');
+		$sql->select('a.published AS state, a.ordering, a.created AS start_date, a.params');
 		$sql->select('a.publish_up AS publish_start_date, a.publish_down AS publish_end_date');
 		$sql->select('c.title AS category, c.published AS cat_state, c.access AS cat_access');
 		$sql->select('CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(":", a.id, a.alias) ELSE a.id END as slug');
