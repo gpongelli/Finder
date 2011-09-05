76779fc579e4651ab2ceafcfb2273ecf51c9a66a
Michael Babker <mbabker@flbab.com>
2011-09-05 16:28:21 +0430

    Add base plugin for com_newsfeeds (ref #17)

---
+++ plg_finder_joomla_newsfeeds/joomla_newsfeeds.php
@@ -0,0 +1,390 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Finder.Joomla_newsfeeds
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_BASE') or die;
+
+// Load the base adapter.
+require_once JPATH_ADMINISTRATOR.'/components/com_finder/helpers/indexer/adapter.php';
+
+/**
+ * Finder adapter for Joomla Newsfeeds.
+ *
+ * @package     Joomla.Plugin
+ * @subpackage  Finder.Joomla_newsfeeds
+ * @since       2.5
+ */
+class plgFinderJoomla_Newsfeeds extends FinderIndexerAdapter
+{
+	/**
+	 * The plugin identifier.
+	 *
+	 * @var    string
+	 * @since  2.5
+	 */
+	protected $context = 'Joomla_Newsfeeds';
+
+	/**
+	 * The sublayout to use when rendering the results.
+	 *
+	 * @var    string
+	 * @since  2.5
+	 */
+	protected $layout = 'newsfeed';
+
+	/**
+	 * The type of content that the adapter indexes.
+	 *
+	 * @var    string
+	 * @since  2.5
+	 */
+	protected $type_title = 'News Feed';
+
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An array that holds the plugin configuration
+	 *
+	 * @return  plgFinderJoomla_Newsfeeds
+	 *
+	 * @since   2.5
+	 */
+	public function __construct(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
+	}
+
+	/**
+	 * Method to reindex the link information for an item that has been saved.
+	 * This event is fired before the data is actually saved so we are going
+	 * to queue the item to be indexed later.
+	 *
+	 * @param   integer  $id  The id of the item.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onBeforeSaveJoomlaNewsfeed($id)
+	{
+		// Queue the item to be reindexed.
+		FinderIndexerQueue::add($this->context, $id, JFactory::getDate()->toMySQL());
+
+		return true;
+	}
+
+	/**
+	 * Method to update the link information for items that have been changed
+	 * from outside the edit screen. This is fired when the item is published,
+	 * unpublished, archived, or unarchived from the list view.
+	 *
+	 * @param   array    $ids       An array of item ids.
+	 * @param   string   $property  The property that is being changed.
+	 * @param   integer  $value     The new value of that property.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onChangeJoomlaNewsfeed($ids, $property, $value)
+	{
+		// Check if we are changing the news feed state.
+		if ($property === 'published')
+		{
+			// The news feed published state is tied to the category published
+			// state so we need to look up all published states before we
+			// change anything.
+			foreach ($ids as $id)
+			{
+				$sql = clone($this->_getStateQuery());
+				$sql->where('a.id = '.(int)$id);
+
+				// Get the published states.
+				$this->_db->setQuery($sql);
+				$item = $this->_db->loadObject();
+
+				// Translate the state.
+				$temp = $this->_translateState($value, $item->cat_state);
+
+				// Update the item.
+				$this->change($id, 'published', $temp);
+			}
+		}
+		// Check if we are changing the news feed access level.
+		else if ($property === 'access')
+		{
+			// The news feed access state is tied to the category
+			// access state so we need to look up all access states
+			// before we change anything.
+			foreach ($ids as $id)
+			{
+				$sql = clone($this->_getStateQuery());
+				$sql->where('a.id = '.(int)$id);
+
+				// Get the published states.
+				$this->_db->setQuery($sql);
+				$item = $this->_db->loadObject();
+
+				// Translate the state.
+				$temp = max($value, $item->cat_access);
+
+				// Update the item.
+				$this->change($id, 'access', $temp);
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Method to update the item link information when the item category is
+	 * changed. This is fired when the item category is published, unpublished,
+	 * or an access level is changed.
+	 *
+	 * @param   array    $ids       An array of item ids.
+	 * @param   string   $property  The property that is being changed.
+	 * @param   integer  $value     The new value of that property.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onChangeJoomlaCategory($ids, $property, $value)
+	{
+		// Check if we are changing the category state.
+		if ($property === 'state')
+		{
+			// The newsfeed published state is tied to the category published
+			// state so we need to look up all published states before we
+			// change anything.
+			foreach ($ids as $id)
+			{
+				$sql = clone($this->_getStateQuery());
+				$sql->where('c.id = '.(int)$id);
+
+				// Get the published states.
+				$this->_db->setQuery($sql);
+				$items = $this->_db->loadObjectList();
+
+				// Adjust the state for each item within the category.
+				foreach ($items as $item)
+				{
+					// Translate the state.
+					$temp = $this->_translateState($item->state, $value);
+
+					// Update the item.
+					$this->change($item->id, 'state', $temp);
+				}
+			}
+		}
+		// Check if we are changing the category access level.
+		else if ($property === 'access')
+		{
+			// The news feed access state is tied to the category access state so
+			// we need to look up all access states before we change anything.
+			foreach ($ids as $id)
+			{
+				$sql = clone($this->_getStateQuery());
+				$sql->where('c.id = '.(int)$id);
+
+				// Get the published states.
+				$this->_db->setQuery($sql);
+				$items = $this->_db->loadObjectList();
+
+				// Adjust the state for each item within the category.
+				foreach ($items as $item)
+				{
+					// Translate the state.
+					$temp = max($item->access, $value);
+
+					// Update the item.
+					$this->change($item->id, 'access', $temp);
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Method to remove the link information for items that have been deleted.
+	 *
+	 * @param   array  $ids  An array of item ids.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	public function onDeleteJoomlaNewsfeed($ids)
+	{
+		// Remove the items.
+		return $this->remove($ids);
+	}
+
+	/**
+	 * Method to index an item. The item must be a FinderIndexerResult object.
+	 *
+	 * @param   FinderIndexerResult  $item  The item to index as an FinderIndexerResult object.
+	 *
+	 * @return  void
+	 *
+	 * @since   2.5
+	 * @throws  Exception on database error.
+	 */
+	protected function index(FinderIndexerResult $item)
+	{
+		// Initialize the item parameters.
+		$registry = new JRegistry;
+		$registry->loadString($item->params);
+		$item->params	= $registry;
+
+		// Build the necessary route and path information.
+		$item->url		= $this->getURL($item->id);
+		$item->route	= NewsfeedsHelperRoute::getNewsfeedRoute($item->slug, $item->catslug);
+		$item->path		= FinderIndexerHelper::getContentPath($item->route);
+
+		// Handle the link to the meta-data.
+		$item->addInstruction(FinderIndexer::META_CONTEXT, 'link');
+
+		// Set the language.
+		$item->language	= FinderIndexerHelper::getDefaultLanguage();
+
+		// Add the type taxonomy data.
+		$item->addTaxonomy('Type', 'News Feed');
+
+		// Add the category taxonomy data.
+		if (!empty($item->category))
+		{
+			$item->addTaxonomy('Category', $item->category, $item->cat_state, $item->cat_access);
+		}
+
+		// Get content extras.
+		FinderIndexerHelper::getContentExtras($item);
+
+		// Index the item.
+		FinderIndexer::index($item);
+	}
+
+	/**
+	 * Method to setup the indexer to be run.
+	 *
+	 * @return  boolean  True on success.
+	 *
+	 * @since   2.5
+	 */
+	protected function setup()
+	{
+		// Load dependent classes.
+		require_once JPATH_SITE.'/includes/application.php';
+		require_once JPATH_SITE.'/components/com_newsfeeds/helpers/route.php';
+
+		return true;
+	}
+
+	/**
+	 * Method to get the SQL query used to retrieve the list of content items.
+	 *
+	 * @param   mixed  $sql  A JDatabaseQuery object or null.
+	 *
+	 * @return  object  A JDatabaseQuery object.
+	 *
+	 * @since   2.5
+	 */
+	protected function getListQuery($sql = null)
+	{
+		$db = JFactory::getDbo();
+		// Check if we can use the supplied SQL query.
+		$sql = is_a($sql, 'JDatabaseQuery') ? $sql : $db->getQuery(true);
+		$sql->select('a.id, a.catid, a.name AS title, a.alias, a.link AS link');
+		$sql->select('a.published AS state, a.ordering, a.publish_up AS start_date, a.params');
+		$sql->select('c.title AS category, c.published AS cat_state, c.access AS cat_access');
+		$sql->select('CASE WHEN CHAR_LENGTH(a.alias) THEN CONCAT_WS(":", a.id, a.alias) ELSE a.id END as slug');
+		$sql->select('CASE WHEN CHAR_LENGTH(c.alias) THEN CONCAT_WS(":", c.id, c.alias) ELSE c.id END as catslug');
+		$sql->from('#__newsfeeds AS a');
+		$sql->join('LEFT', '#__categories AS c ON c.id = a.catid');
+
+		return $sql;
+	}
+
+	/**
+	 * Method to get the URL for the item. The URL is how we look up the link
+	 * in the Finder index.
+	 *
+	 * @param   mixed  $id  The id of the item.
+	 *
+	 * @return  string  The URL of the item.
+	 *
+	 * @since   2.5
+	 */
+	protected function getURL($id)
+	{
+		return 'index.php?option=com_newsfeeds&view=newsfeed&id='.$id;
+	}
+
+	/**
+	 * Method to translate the native content states into states that the
+	 * indexer can use.
+	 *
+	 * @param   integer  $newsfeed  The newsfeed state.
+	 * @param   integer  $category  The category state.
+	 *
+	 * @return  integer  The translated indexer state.
+	 *
+	 * @since   2.5
+	 */
+	private function _translateState($newsfeed, $category)
+	{
+		// If category is present, factor in its states as well.
+		if ($category !== null)
+		{
+			if ($category == 0)
+			{
+				$newsfeed = 0;
+			}
+		}
+
+		// Translate the state.
+		switch ($newsfeed)
+		{
+			// Unpublished.
+			case 0:
+				return 0;
+
+			// Published.
+			default:
+			case 1:
+				return 1;
+		}
+	}
+
+	/**
+	 * Method to get a SQL query to load the published and access states for
+	 * an article and category.
+	 *
+	 * @return  object  A JDatabaseQuery object.
+	 *
+	 * @since   2.5
+	 */
+	private function _getStateQuery()
+	{
+		$sql = $this->_db->getQuery(true);
+		$sql->select('a.id');
+		$sql->select('a.published AS state, c.published AS cat_state');
+		$sql->select('0 AS access, c.access AS cat_access');
+		$sql->from('#__newsfeeds AS a');
+		$sql->join('LEFT', '#__categories AS c ON c.id = a.catid');
+
+		return $sql;
+	}
+}
