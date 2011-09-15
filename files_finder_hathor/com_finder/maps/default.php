74f8a8e08bfa968c2ccd63f08d4ff6811783f332
Michael Babker <mbabker@flbab.com>
2011-09-15 15:55:55 +0430

    Edits to admin maps view
    Added Hathor override and standardized the foreach processing

--- files_finder_hathor/com_finder/maps/default.php
+++ files_finder_hathor/com_finder/maps/default.php
@@ -1,13 +1,17 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_finder
+ * @subpackage  Templates.hathor
  *
  * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
+// no direct access
 defined('_JEXEC') or die;
+
+JHtml::_('behavior.tooltip');
+JHtml::_('behavior.multiselect');
 
 $listOrder	= $this->escape($this->state->get('list.ordering'));
 $listDirn	= $this->escape($this->state->get('list.direction'));
@@ -30,62 +34,71 @@ Joomla.submitbutton = function(pressbutton) {
 </script>
 <form action="<?php echo JRoute::_('index.php?option=com_finder&view=maps');?>" method="post" name="adminForm" id="adminForm">
 	<fieldset id="filter-bar">
-		<div class="filter-search fltlft">
+	<legend class="element-invisible"><?php echo JText::sprintf('COM_FINDER_SEARCH_LABEL', JText::_('COM_FINDER_MAPS')); ?></legend>
+		<div class="filter-search">
 			<label class="filter-search-lbl" for="filter_search"><?php echo JText::sprintf('COM_FINDER_SEARCH_LABEL', JText::_('COM_FINDER_MAPS')); ?></label>
 			<input type="text" name="filter_search" id="filter_search" value="<?php echo $this->escape($this->state->get('filter.search')); ?>" title="<?php echo JText::_('COM_FINDER_FILTER_SEARCH_DESCRIPTION'); ?>" />
 			<button type="submit" class="btn"><?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?></button>
 			<button type="button" onclick="document.id('filter_search').value='';this.form.submit();"><?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?></button>
 		</div>
-		<div class="filter-select fltrt">
-			<select name="filter_branch" class="inputbox" onchange="this.form.submit()" id="filter_branch">
+
+		<div class="filter-select">
+			<label class="selectlabel" for="filter_branch"><?php echo JText::sprintf('COM_FINDER_FILTER_BY', JText::_('COM_FINDER_MAPS')); ?></label>
+			<select name="filter_branch" class="inputbox" id="filter_branch">
 				<?php echo JHtml::_('select.options', JHtml::_('finder.mapslist'), 'value', 'text', $this->state->get('filter.branch'), true);?>
 			</select>
-			<select name="filter_state" class="inputbox" onchange="this.form.submit()">
+
+			<label class="selectlabel" for="filter_state"><?php echo JText::_('COM_FINDER_INDEX_FILTER_BY_STATE'); ?></label>
+			<select name="filter_state" class="inputbox" id="filter_state">
 				<option value=""><?php echo JText::_('COM_FINDER_INDEX_FILTER_BY_STATE');?></option>
 				<?php echo JHtml::_('select.options', JHtml::_('finder.statelist'), 'value', 'text', $this->state->get('filter.state'), true);?>
 			</select>
+
+			<button type="submit" id="filter-go">
+				<?php echo JText::_('JSUBMIT'); ?>
+			</button>
 		</div>
 	</fieldset>
 	<div class="clr"> </div>
 
-	<table class="adminlist" style="clear: both;">
+	<table class="adminlist">
 		<thead>
 			<tr>
-				<th width="1%">
+				<th class="checkmark-col">
 					<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
 				</th>
-				<th class="nowrap">
+				<th class="title">
 					<?php echo JHtml::_('grid.sort', 'JGLOBAL_TITLE', 'a.title', $listDirn, $listOrder); ?>
 				</th>
-				<th class="nowrap" width="10%">
+				<th class="nowrap state-col">
 					<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
 				</th>
 			</tr>
 		</thead>
+
 		<tbody>
-			<?php if (count($this->items) == 0): ?>
+		<?php if (count($this->items) == 0): ?>
 			<tr class="row0">
 				<td class="center" colspan="5">
 					<?php echo JText::_('COM_FINDER_MAPS_NO_CONTENT'); ?>
 				</td>
 			</tr>
-			<?php endif; ?>
-			<?php if ($this->state->get('filter.branch') != 1) : ?>
+		<?php endif; ?>
+		<?php if ($this->state->get('filter.branch') != 1) : ?>
 			<tr class="row1">
 				<td colspan="5" class="center">
 					<a href="#" onclick="document.id('filter_branch').value='1';document.adminForm.submit();">
 						<?php echo JText::_('COM_FINDER_MAPS_RETURN_TO_BRANCHES'); ?></a>
 				</td>
 			</tr>
-			<?php endif; ?>
+		<?php endif; ?>
 
-			<?php $canChange	= JFactory::getUser()->authorise('core.manage',	'com_finder'); ?>
-			<?php foreach ($this->items as $i => $item): ?>
-
+		<?php $canChange	= JFactory::getUser()->authorise('core.manage',	'com_finder'); ?>
+		<?php foreach ($this->items as $i => $item) :?>
 			<tr class="row<?php echo $i % 2; ?>">
-				<td class="center">
+				<th class="center">
 					<?php echo JHtml::_('grid.id', $i, $item->id); ?>
-				</td>
+				</th>
 				<td>
 					<?php
 						$key = 'COM_FINDER_TYPE_S_'.strtoupper(str_replace(' ', '_', $item->title));
@@ -102,25 +115,20 @@ Joomla.submitbutton = function(pressbutton) {
 					<?php elseif ($item->num_nodes > 0) : ?>
 						<small>(<?php echo $item->num_nodes; ?>)</small>
 					<?php endif; ?>
-				</td>
-				<td class="center nowrap">
+									</td>
+				<td class="center">
 					<?php echo JHtml::_('jgrid.published', $item->state, $i, 'maps.', $canChange, 'cb'); ?>
 				</td>
 			</tr>
 			<?php endforeach; ?>
 		</tbody>
-		<tfoot>
-			<tr>
-				<td colspan="9" class="nowrap">
-					<?php echo $this->pagination->getListFooter(); ?>
-				</td>
-			</tr>
-		</tfoot>
 	</table>
 
-	<input type="hidden" name="task" value="display" />
+	<?php echo $this->pagination->getListFooter(); ?>
+
+	<input type="hidden" name="task" value="" />
 	<input type="hidden" name="boxchecked" value="0" />
-	<input type="hidden" name="filter_order" value="<?php echo $listOrder ?>" />
-	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn ?>" />
+	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>" />
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>" />
 	<?php echo JHtml::_('form.token'); ?>
 </form>
