a89b82d470d9f24c90919fc58f2d477371cb3430
Michael Babker <mbabker@flbab.com>
2011-09-17 21:44:46 +0430

    Remove code for methods that have been converted or not carried over

--- plg_libraries/jx.php
+++ plg_libraries/jx.php
@@ -72,9 +72,6 @@ class plgSystemJX extends JPlugin
 						$state	= JRequest::getInt('published');
 						$access	= JRequest::getInt('access');
 
-						// Fire the onBeforeSaveJoomlaCategory event.
-						$this->_fireEvent('onBeforeSaveJoomlaCategory', array($id));
-
 						// Fire the onChangeJoomlaCategory event for the published state.
 						$this->_fireEvent('onChangeJoomlaCategory', array($cid, 'published', $state));
 
@@ -147,18 +144,6 @@ class plgSystemJX extends JPlugin
 				// Check for tasks that we care about.
 				switch ($task)
 				{
-					// Handle save tasks.
-					case 'apply':
-					case 'save':
-					{
-						// Get the item id.
-						$id = JRequest::getInt('id');
-
-						// Fire the onBeforeSaveJoomlaContact event.
-						$this->_fireEvent('onBeforeSaveJoomlaContact', array($id));
-						break;
-					}
-
 					// Handle published state tasks.
 					case 'publish':
 					case 'unpublish':
@@ -208,18 +193,6 @@ class plgSystemJX extends JPlugin
 
 						// Fire the onChangeJoomlaContact event.
 						$this->_fireEvent('onChangeJoomlaContact', array($cid, 'access', $value));
-						break;
-					}
-
-					// Handle remove task.
-					case 'remove':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Fire the onDeleteJoomlaContact event.
-						$this->_fireEvent('onDeleteJoomlaContact', array($cid));
 						break;
 					}
 				}
@@ -291,200 +264,10 @@ class plgSystemJX extends JPlugin
 						$this->_fireEvent('onChangeJoomlaArticle', array($cid, 'access', $value));
 						break;
 					}
-
-					// Handle remove task.
-					case 'remove':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Fire the onTrashJoomlaArticle event.
-						$this->_fireEvent('onTrashJoomlaArticle', array($cid));
-						break;
-					}
 				}
 
 				// Break from com_content.
 				break;
-			}
-
-			// Handle com_labels.
-			case 'com_labels':
-			{
-				// Check for tasks that we care about.
-				switch ($task)
-				{
-					// Handle published state tasks.
-					case 'labels.publish':
-					case 'labels.unpublish':
-					case 'labels.archive':
-					case 'labels.unarchive':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Get the new value.
-						switch ($task) {
-							default:
-							case 'labels.publish':
-								$value = 1;
-								break;
-							case 'labels.unarchive':
-							case 'labels.unpublish':
-								$value = 0;
-								break;
-							case 'labels.archive':
-								$value = -1;
-								break;
-						}
-
-						// Fire the onChangeLabelsLabel event.
-						$this->_fireEvent('onChangeLabelsLabel', array($cid, 'state', $value));
-						break;
-					}
-
-					// Handle remove task.
-					case 'delete':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Fire the onDeleteLabelsLabel event.
-						$this->_fireEvent('onDeleteLabelsLabel', array($cid));
-						break;
-					}
-				}
-
-				// Break from com_content.
-				break;
-			}
-
-			// Handle com_sections.
-			case 'com_sections':
-			{
-				// Check for tasks that we care about.
-				switch ($task)
-				{
-					// Handle save tasks.
-					case 'apply':
-					case 'save':
-					{
-						// Get the item id.
-						$id = JRequest::getInt('id');
-
-						// Ignore new items.
-						if (!$id) {
-							break;
-						}
-
-						// Prepare values.
-						$cid	= array($id);
-						$state	= JRequest::getInt('published');
-						$access	= JRequest::getInt('access');
-
-						// Fire the onBeforeSaveJoomlaSection event.
-						$this->_fireEvent('onBeforeSaveJoomlaSection', array($id));
-
-						// Fire the onChangeJoomlaSection event for the published state.
-						$this->_fireEvent('onChangeJoomlaSection', array($cid, 'published', $state));
-
-						// Fire the onChangeJoomlaSection event for the access level.
-						$this->_fireEvent('onChangeJoomlaSection', array($cid, 'access', $access));
-						break;
-					}
-
-					// Handle published state tasks.
-					case 'publish':
-					case 'unpublish':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Get the new value.
-						switch ($task) {
-							default:
-							case 'publish':
-								$value = 1;
-								break;
-							case 'unpublish':
-								$value = 0;
-								break;
-						}
-
-						// Fire the onChangeJoomlaSection event.
-						$this->_fireEvent('onChangeJoomlaSection', array($cid, 'published', $value));
-						break;
-					}
-
-					// Handle access state tasks.
-					case 'accesspublic':
-					case 'accessregistered':
-					case 'accessspecial':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Get the new value.
-						switch ($task) {
-							default:
-							case 'accesspublic':
-								$value = 0;
-								break;
-							case 'accessregistered':
-								$value = 1;
-								break;
-							case 'accessspecial':
-								$value = 2;
-								break;
-						}
-
-						// Fire the onChangeJoomlaSection event.
-						$this->_fireEvent('onChangeJoomlaSection', array($cid, 'access', $value));
-						break;
-					}
-
-				}
-
-				// Break from com_sections.
-				break;
-			}
-
-			// Handle com_trash.
-			case 'com_trash':
-			{
-				// Check if the user is authrozed to manage com_trash.
-				if (!$user->authorize('com_trash', 'manage')) {
-					return true;
-				}
-
-				// Check if we are deleting content.
-				if ($task === 'delete' && JRequest::getCmd('type') == 'content')
-				{
-					// Get the ids.
-					$cid = JRequest::getVar('cid', array(), 'method', 'array');
-					JArrayHelper::toInteger($cid);
-
-					// Fire the onDeleteJoomlaArticle event.
-					$this->_fireEvent('onDeleteJoomlaArticle', array($cid));
-					break;
-				}
-
-				// Check if we are restoring content.
-				if ($task === 'restore' && JRequest::getCmd('type') == 'content')
-				{
-					// Get the ids.
-					$cid = JRequest::getVar('cid', array(), 'method', 'array');
-					JArrayHelper::toInteger($cid);
-
-					// Fire the onChangeJoomlaArticle event.
-					$this->_fireEvent('onChangeJoomlaArticle', array($cid, 'state', 0));
-					break;
-				}
 			}
 
 			// Handle com_weblinks.
@@ -493,18 +276,6 @@ class plgSystemJX extends JPlugin
 				// Check for tasks that we care about.
 				switch ($task)
 				{
-					// Handle save tasks.
-					case 'apply':
-					case 'save':
-					{
-						// Get the item id.
-						$id = JRequest::getInt('id');
-
-						// Fire the onBeforeSaveJoomlaWeblink event.
-						$this->_fireEvent('onBeforeSaveJoomlaWeblink', array($id));
-						break;
-					}
-
 					// Handle published state tasks.
 					case 'publish':
 					case 'unpublish':
@@ -528,60 +299,12 @@ class plgSystemJX extends JPlugin
 						$this->_fireEvent('onChangeJoomlaWeblink', array($cid, 'published', $value));
 						break;
 					}
-
-					// Handle remove task.
-					case 'remove':
-					{
-						// Get the ids.
-						$cid = JRequest::getVar('cid', array(), 'method', 'array');
-						JArrayHelper::toInteger($cid);
-
-						// Fire the onDeleteJoomlaWeblink event.
-						$this->_fireEvent('onDeleteJoomlaWeblink', array($cid));
-						break;
-					}
 				}
 
 				// Break from com_weblinks.
 				break;
 			}
 		}
-	}
-
-	/**
-	 * Method to catch the onAfterContentSave event. We catch this event
-	 * because there is no other reliable way to get in between the user
-	 * clicking the save/apply button the content being written to the
-	 * database.
-	 *
-	 * @param	object		The item as a JTableContent object.
-	 * @param	boolean		Flag to indicate whether the item is new.
-	 * @return	boolean		True on success.
-	 * @throws	Exception on database error.
-	 */
-	public function onAfterContentSave(&$item, $isNew)
-	{
-		if (strtolower(JRequest::getCmd('option')) == 'com_content') {
-			// Fire the onSaveJoomlaArticle event.
-			$this->_fireEvent('onSaveJoomlaArticle', array($item->id));
-		}
-	}
-
-	/**
-	 * Method to catch the onAfterLabelSave event. We catch this event
-	 * because there is no other reliable way to get in between the user
-	 * clicking the save/apply button the content being written to the
-	 * database.
-	 *
-	 * @param	object		The item as a LabelsTableLabels object.
-	 * @param	boolean		Flag to indicate whether the item is new.
-	 * @return	boolean		True on success.
-	 * @throws	Exception on database error.
-	 */
-	public function onAfterLabelSave(&$item, $isNew)
-	{
-		// Fire the onSaveLabelsLabel event.
-		$this->_fireEvent('onSaveLabelsLabel', array($item->label_id));
 	}
 
 	/**
