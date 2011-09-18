d2010c5e4eb60d49934e8b17dc990db7732723c2
Michael Babker <mbabker@flbab.com>
2011-09-18 19:54:57 +0430

    Rename Contacts plugin

--- plg_finder_contacts/contacts.php
+++ plg_finder_contacts/contacts.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  Finder.Joomla_contacts
+ * @subpackage  Finder.Contacts
  *
  * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
@@ -16,10 +16,10 @@ require_once JPATH_ADMINISTRATOR.'/components/com_finder/helpers/indexer/adapter
  * Finder adapter for Joomla Contacts.
  *
  * @package     Joomla.Plugin
- * @subpackage  Finder.Joomla_contacts
+ * @subpackage  Finder.Contacts
  * @since       2.5
  */
-class plgFinderJoomla_Contacts extends FinderIndexerAdapter
+class plgFinderContacts extends FinderIndexerAdapter
 {
 	/**
 	 * The plugin identifier.
@@ -27,7 +27,7 @@ class plgFinderJoomla_Contacts extends FinderIndexerAdapter
 	 * @var    string
 	 * @since  2.5
 	 */
-	protected $context = 'Joomla_Contacts';
+	protected $context = 'Contacts';
 
 	/**
 	 * The sublayout to use when rendering the results.
@@ -51,7 +51,7 @@ class plgFinderJoomla_Contacts extends FinderIndexerAdapter
 	 * @param   object  &$subject  The object to observe
 	 * @param   array   $config    An array that holds the plugin configuration
 	 *
-	 * @return  plgFinderJoomla_Contacts
+	 * @return  plgFinderContacts
 	 *
 	 * @since   2.5
 	 */
